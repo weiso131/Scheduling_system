5e4ec2ee2b266db96c1e6750c2a1c65fa24825b6
weiso131 <blackfire106.1231@gmail.com>
2024-08-02 11:07:03 +0800

    Input year, month

--- src/app.py
+++ src/app.py
@@ -1,12 +1,16 @@
+from datetime import datetime, date
+
 from flask import Flask, request, jsonify, redirect, url_for, render_template
 from scheduling import *
 
 DEPARTMNT = 0
 EMPLOYEE = 1
 
+year = -1
 month = 7
+start_day = 1
 
-format = SchedualFormat(day_nums=31, period=2, start_day=1)
+format = SchedualFormat(day_nums=31, period=2, start_day=start_day)
 format.set_manpower_in_week(week_day=6, manpower=0, period=[1])
 format.set_manpower_in_week(week_day=7, manpower=0, period=[0, 1])
 
@@ -16,14 +20,29 @@ my_schedual.employees = [Employee("teddy", "82", format), Employee("weiso", "83"
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return redirect(url_for('show_employees_status'))
+    global year, month, start_day
+    if request.method == 'GET':
+        now = datetime.now()
+        return render_template('home.html', \
+                               default_data={"year" : str(now.year), "month" : str(now.month)})
+    else:
+        year = int(request.form['year'])
+        month = int(request.form['month'])
+
+        specific_day = date(year, month, 1)
+        start_day = (specific_day.weekday() + 1 ) % 7
+
+        
+        return redirect(url_for('show_status'))
 
 @app.route('/show_status')
 def show_status():
-    return render_template('home.html', employees=my_schedual.get_employee_json(), \
-                           departments=my_schedual.get_department_json(), target_class=EMPLOYEE)
+    global month
+    return render_template('show_status.html', employees=my_schedual.get_employee_json(), \
+                           departments=my_schedual.get_department_json(), target_class=EMPLOYEE, 
+                           title=f"{year}年 {month}月 班表")
 
 
 @app.route('/edit/<int:target_class>/<int:id>')
@@ -65,7 +84,8 @@ def build_schedule():
     my_schedual.reload()
     my_schedual.bind_schedual()
     my_schedual.basic_schedual(last_month=1)
-    return render_template("show_schedule.html", month=month, days=list(range(1, format.day_nums + 1)), employees=my_schedual.get_employee_json(use_state=True))
+    return render_template("show_schedule.html", month=month, days=list(range(1, format.day_nums + 1)), \
+                           employees=my_schedual.get_employee_json(use_state=True), title=f"{year}年 {month}月 班表")
     
 
 if __name__ == '__main__':
